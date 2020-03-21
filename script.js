86958439be8799179accc17f8bb5645188b14908
Vanessa Chen <vanchen07@gmail.com>
2020-03-21 09:22:26 -0700

    add scroll event listener and showLoading function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const postsContainer = document.getElementById('posts-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 async function getPosts() {
@@ -27,8 +27,30 @@ async function showPosts() {
             </div>
         `;
 
-        postsContainer.appendChild(postEl);
+        postsContainer.appendChild(postEl)
     });
 }
 
+function showLoading() {
+    loading.classList.add('show');
+
+    setTimeout(() => {
+        loading.classList.remove('show');
+
+        setTimeout(() => {
+            page++;
+            showPosts();
+        }, 300);
+
+    }, 1000);
+}
+
 showPosts();
+
+window.addEventListener("scroll", () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+    if (scrollTop + clientHeight > scrollHeight - 5) {
+        showLoading();
+    }
+});
